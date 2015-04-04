2222bf5145534734cfc1f670d01035dc8b0094f8
Paweł Kapalla <pkapalla@xesenix.pl>
2015-04-04 22:51:02 +0200

    move library to seperate repository

--- lib/BDF2/Avatar/Provider/AvatarCompositProviderInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace BDF2\Avatar\Provider;
-
-interface AvatarCompositProviderInterface extends AvatarProviderInterface {
-	function register(AvatarProviderInterface $provider);
-}
--- lib/BDF2/Avatar/Provider/AvatarProviderInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace BDF2\Avatar\Provider;
-
-interface AvatarProviderInterface {
-	function getAvatar($id, $size);
-}
